5570177f1251238a80c1ac99363665cea3d8c7b7
gkosulkar <148052131+gkosulkar@users.noreply.github.com>
2023-10-23 12:45:30 +0530

    Add files via upload

---
+++ 54321.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{ 
+int r,c,sp;
+
+for (r=5 ;r>=1;r--)
+{
+	for (sp=5;sp>=1;sp--)
+	
+	{
+		printf( " ");
+	}
+	for(c=1;c<=r;c++)
+	{
+		printf("%d",c);
+	}
+	printf("\n");
+	}
+		
+	
+
+}
