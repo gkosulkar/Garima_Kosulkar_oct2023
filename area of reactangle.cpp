9aa2454fc42957b79287e5ab46c5af23b9bfda9a
gkosulkar <148052131+gkosulkar@users.noreply.github.com>
2023-11-02 21:27:28 +0530

    Add files via upload

---
+++ area of reactangle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int r;   
+	printf("enter the value of r",r);
+	scanf("%d",&r) ;
+	
+	float circumference;
+		 circumference=2*3.14*r;
+	 printf("circumference of circle= %f",circumference);
+	
+}          
