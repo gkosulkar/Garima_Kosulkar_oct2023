c33b941d6e5409646e4d7ec92b6df343c66bb4e9
gkosulkar <148052131+gkosulkar@users.noreply.github.com>
2023-11-02 22:10:52 +0530

    Add files via upload

---
+++ password.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int pass,x=10;
+	printf("enter a password");
+	scanf("%d",&pass);
+	if (pass==1234)
+	{
+		printf("correct password");
+	}
+	else 
+	
+	                                                                                                                   
+	{
+		printf("incorrect password");
+	}
+}
