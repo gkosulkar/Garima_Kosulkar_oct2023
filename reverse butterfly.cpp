f51ae6b1283e9f645461fe24ac5de617633458c7
gkosulkar <148052131+gkosulkar@users.noreply.github.com>
2023-11-02 22:19:27 +0530

    Add files via upload

---
+++ reverse butterfly.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+   {
+	 int i,j,k,n;
+	 printf("enter the value for n");
+	 scanf("%d",&n);
+	 for(i=1;i<=n;i++)
+	   {
+	 	for (j=1;j<=(n+1)-i;j++)
+	 	   {
+	 		printf("*");
+		   }
+		 for(k=1;k<i;k++)
+		   {
+		 	printf(" ");
+		   }
+		 for(j=1;j<=(n+1)-i;j++)
+		    {
+		 	printf("*");
+		    }
+		 printf("\n");
+	   }
+	 
+	}
+	 
+        
+
