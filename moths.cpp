36992b994c518920f11ba6899dedf992739b66d4
gkosulkar <148052131+gkosulkar@users.noreply.github.com>
2023-11-02 22:01:42 +0530

    Add files via upload

---
+++ moths.cpp
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the number");
+	scanf("%d",&num);
+	if(num==1)
+	{
+		printf("januvary");
+	}
+	else if (num==2)
+     {
+     	printf("februvary");
+	}
+	else if (num==3)
+	{
+		printf(" march");
+		}
+		else if (num==4)
+		{
+			printf("april");     
+			}
+			else if (num==5)
+			{
+				printf("may");
+				}	
+				else if (num==6)
+				{
+					printf(" june");
+				}
+				else if (num==7)
+				{
+					printf("jully");
+				}
+				else if (num==8)
+				{
+					printf("agust");
+				}
+				else if (num==9)
+				{
+					printf ("sept");
+				}
+				else if (num==10)
+				{
+					printf("oct");
+				}
+				else if (num==11)
+				{
+					printf("nov");
+				}
+				else if (num==12)
+				{
+					printf("december");
+				}
+				printf("\n");
+}
