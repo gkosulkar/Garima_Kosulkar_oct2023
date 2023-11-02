ef226829c7a9791dbffdd86728306a5eda43e2ad
gkosulkar <148052131+gkosulkar@users.noreply.github.com>
2023-11-02 22:28:20 +0530

    Add files via upload

---
+++ star pattern.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{ 
+int a,b,sp;
+
+for (a=1;a<=5;a++)
+{
+	for (sp=5;sp>=b;sp--)
+	
+	{
+		printf(" ");
+	}
+	for(b=1;b<=a;b++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	}
+		
+	
+
+}
